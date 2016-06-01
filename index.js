c5bfff9a1bd2b4e5bbfa509599235c6254bbb3cf
Renato Zannon <renato@bankfacil.com.br>
2016-06-01 20:41:36 -0300

    Support outputPaths (multiple output files)

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
 var StylusCompiler = require('broccoli-stylus-single');
+var path = require('path');
+var mergeTrees = require('broccoli-merge-trees');
+var merge = require('merge');
 
-function StylusPlugin(options) {
+function StylusPlugin(optionsFn) {
   this.name = 'ember-cli-stylus';
   this.ext = 'styl';
-  options = options || {};
-  options.inputFile = options.inputFile || 'app.styl';
-  options.outputFile = options.outputFile || 'app.css';
-  if (options.sourceMap) {
-    options.sourceComments = 'map';
-    options.sourceMap = options.outputFile + '.map';
-  }
-  this.options = options;
+
+  this.optionsFn = optionsFn;
 };
 
-StylusPlugin.prototype.toTree = function(tree, inputPath, outputPath) {
+StylusPlugin.prototype.toTree = function(tree, inputPath, outputPath, inputOptions) {
+  var options = merge({}, this.optionsFn(), inputOptions);
+
   var trees = [tree];
-  if (this.options.includePaths) trees = trees.concat(this.options.includePaths);
-  inputPath += '/' + this.options.inputFile;
-  outputPath += '/' + this.options.outputFile;
-  return new StylusCompiler(trees, inputPath, outputPath, this.options);
+  if (options.includePaths) trees = trees.concat(options.includePaths);
+
+  var paths = options.outputPaths;
+  var outputTrees = Object.keys(paths).map(function(file) {
+    var output = paths[file];
+    var input = path.join(inputPath, file + '.styl');
+    var optionsForTree = merge({}, options);
+
+    if (optionsForTree.sourceMap) {
+      optionsForTree.sourceComments = 'map';
+      optionsForTree.sourceMap = output + '.map';
+    }
+
+    return new StylusCompiler(trees, input, output, options);
+  });
+
+  return mergeTrees(outputTrees);
 };
 
-function EmberCLIStylus(project) {
-  this.project = project;
-  this.name = 'Ember CLI Stylus';
-}
+module.exports = {
+  name: 'ember-cli-stylus',
 
-EmberCLIStylus.prototype.treeFor = function treeFor(type) {
-};
+  stylusOptions: function() {
+    var env = process.env.EMBER_ENV;
+    var options = (this.app && this.app.options && this.app.options.stylusOptions) || {};
 
-EmberCLIStylus.prototype.included = function included(app) {
-  var options = app.options.stylusOptions || {};
-  if ((options.sourceMap === undefined) && (app.env == 'development')) {
-    options.sourcemap = {
-      inline: true
-    };
-    options.cache = false;
+    if ((options.sourceMap === undefined) && (env == 'development')) {
+      options.cache = false;
+      options.sourceMap = {
+        inline: true
+      };
+    }
+
+    return options;
+  },
+
+  setupPreprocessorRegistry: function(type, registry) {
+    registry.add('css', new StylusPlugin(this.stylusOptions.bind(this)));
+  },
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    // see: https://github.com/ember-cli/ember-cli/issues/3718
+    if (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+
+    this.app = app;
   }
-  options.outputFile = options.outputFile || this.project.name() + '.css';
-  app.registry.add('css', new StylusPlugin(options));
 };
-
-module.exports = EmberCLIStylus;
